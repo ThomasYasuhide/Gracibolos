8834c812b84e14e1556d71ee482a4106a437aa1b
Yasuhide <Yasuhide@192.168.1.38>
2016-02-26 16:47:36 -0300

    Documentando a classe.

--- Gracibolos/src/main/java/br/com/gracibolos/controller/LoginController.java
+++ Gracibolos/src/main/java/br/com/gracibolos/controller/LoginController.java
@@ -23,6 +23,7 @@ public class LoginController {
 	@RequestMapping("/loginview")
 	public ModelAndView loginView(HttpSession session){
 		
+		//Direciona para a pagina index dentro da WEB-INF
 		return new ModelAndView("index");
 	}
 	
@@ -30,24 +31,31 @@ public class LoginController {
 	@RequestMapping("/login")
 	public ModelAndView login(Colaborador colaborador, HttpSession session, HttpServletResponse response, HttpServletRequest request){
 		
-		page = "index";
-		status = false;
-		servidor = false;
+		//Declaração de valores padrões para as variaveis.
+		page = "index"; //Página padrão para redirecionamento
+		status = false; //Usuario ou senha inválidos.
+		servidor = false; //Servidor offline.
 		
 		try {
+			//Passa os dados da view por parametro, e pesquisa no banco de dados, caso o colaborador exista, popula o colaborador com as informações.
 			colaborador = Login.login(colaborador);
 			
+			//Altera o status do servidor para online.
 			servidor = true;
 			
+			//Verifica se o colaborador possuí algum numero de ID, senão apenas redireciona para pagina padrão
 	        if(colaborador.getId() != null){
 	        	
+	        	//Cria uma nova sessão armazendo os dados do colaborador.
 	            session.setMaxInactiveInterval(30*60);
 	            session.setAttribute("id", colaborador.getId());
 	            session.setAttribute("nome", colaborador.getNome());
 	            session.setAttribute("nivel", colaborador.getNivel());
 	            
+	            //Verifica se a opção de lembrar senha está ativa.
 	            if(colaborador.isLembrarsenha()){
-	            	System.out.println("######################## CRIANDO COOKIES #########################");
+	            	//Armazena os dados do usuário em cookies
+	            	System.out.println("Usuário optou por lembrar sua senha.");
 	            	Cookie usuario = new Cookie("usuario", colaborador.getUsuario());
 	            	Cookie senha = new Cookie("senha", colaborador.getSenha());
 	            	Cookie lembrarsenha = new Cookie("lembrarsenha", String.valueOf(colaborador.isLembrarsenha()));
@@ -55,14 +63,15 @@ public class LoginController {
 	                response.addCookie(senha);
 	                response.addCookie(lembrarsenha);
 	            }else{
-	            	System.out.println("######################## EXCLUIR COOKIES #########################");
+	            	//Limpa os dados dos cookies
+	            	System.out.println("Usuário optou por não lembrar senha.");
 	            	Cookie[] cookies = request.getCookies();
 	                for(int i = 0; i< cookies.length ; ++i){
                         //Verifica se existe algum cookie
 	                	if(cookies != null){
-	                		//Verifica se o cookie armazena algum usuário ou senha
+	                		//Verifica se o cookie armazena algum usuário, senha, ou lembrarsenha
                         	if(cookies[i].getName().equals("usuario") || cookies[i].getName().equals("senha") || cookies[i].getName().equals("lembrarsenha")){
-			                	//Deleta o cookie
+			                	//Deleta os cookies
                         		cookies[i].setMaxAge(0);
 		                        response.addCookie(cookies[i]);
                         	}
@@ -70,22 +79,21 @@ public class LoginController {
 	                } 
 	            }
 	            
+	            //Verifica o nível de acesso do colaborador e direciona para a página de acesso.
 	            switch(colaborador.getNivel()){
 	                case 1:
-	                    System.out.println("Nível de privilégio 1");
-	                    status = true;
-	                    page = "redirect:operacional/clientes";
+	                    System.out.println("Nível de acesso operador");
+	                    status = true; //Altera o status de login válido
+	                    page = "redirect:operacional-clientes"; //Direciona para a pagina do operador/clientes
 	                    break;
 	                
 	                case 2:
-	                    System.out.println("Nível de privilégio 2");
-	                    status = true;
-	                    page = "redirect:administrativo-dashboard";
+	                    System.out.println("Nível de acesso administrador");
+	                    status = true; //Altera o status de login válido
+	                    page = "redirect:administrativo-dashboard";  //Direciona para a pagina do administrador/dashboard
 	                    break;
 	            }
 	            
-	        }else{
-	            page = "index";
 	        }
 
 		} catch (Exception e) {
@@ -97,9 +105,12 @@ public class LoginController {
         mv.setViewName(page);
         
         if(page == "index"){
+        	//Adicionar os objetos
         	mv.addObject("status",status);
             mv.addObject("servidor",servidor);
         }
+        
+        //Adicionar os objetos
         mv.addObject("colaborador",colaborador);
         
 		return mv;
@@ -108,6 +119,7 @@ public class LoginController {
 	@RequestMapping("logout")
 	public ModelAndView logout(HttpSession session) {
 		
+		//Destroi a sessão do usuário
 		System.out.println("Usuário fez logout");
 		session.invalidate();
 		return new ModelAndView("index");
