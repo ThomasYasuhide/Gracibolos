c046027ac25dab4f86213b1ae6223f5ca6fe7fb8
Rogerio <Rogerio@192.168.1.107>
2016-04-13 12:59:22 -0300

    Implementação da Classe controller RestController

--- Gracibolos/src/main/java/br/com/gracibolos/controller/ProdutoRestController.java
+++ Gracibolos/src/main/java/br/com/gracibolos/controller/ProdutoRestController.java
@@ -2,14 +2,9 @@ package br.com.gracibolos.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.gracibolos.jdbc.dao.ProdutoDao;
