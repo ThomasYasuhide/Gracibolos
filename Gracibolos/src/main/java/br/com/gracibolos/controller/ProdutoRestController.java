cd8f211d71ee8e1ea57ed2665e11334f2802f9ef
Rogerio <Rogerio@t430u>
2016-04-14 08:25:28 -0300

    otmização restcontroller

--- Gracibolos/src/main/java/br/com/gracibolos/controller/ProdutoRestController.java
+++ Gracibolos/src/main/java/br/com/gracibolos/controller/ProdutoRestController.java
@@ -2,7 +2,6 @@ package br.com.gracibolos.controller;
 
 import java.util.List;
 
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,29 +13,31 @@ import br.com.gracibolos.jdbc.model.Produto;
 @RestController
 public class ProdutoRestController {
 	
+	private Produto p;
 	private ProdutoDao dao;
 	private List<Produto> produtosList;
 	
 	
-	@RequestMapping(value = "/rest-produtos", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/rest-produtos", method = RequestMethod.GET, headers="Accept=application/json")
 	public List<Produto> listAllProdutos() {    
 		try {
 			dao = new ProdutoDao();
 			produtosList = dao.listar();
 		} catch (Exception e) {
+			System.out.println("ERRO - rest produtos, lista de produtos.");
 			e.printStackTrace();
 		}
 		return produtosList;
     }
 	
-	@RequestMapping(value = "/rest-produtos/{id}", method = RequestMethod.GET)  
+	@RequestMapping(value = "/rest-produtos/{id}", method = RequestMethod.GET, headers="Accept=application/json")  
 	 public Produto findById(@PathVariable int id) {  
 		dao = new ProdutoDao();
-		Produto p = new Produto();
+		p = new Produto();
 		try {
 			p = dao.pesquisarPorId(id);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
+			System.out.println("ERRO - rest produtos, busca produtos por id.");
 			e.printStackTrace();
 		}    
 	  return p;  
