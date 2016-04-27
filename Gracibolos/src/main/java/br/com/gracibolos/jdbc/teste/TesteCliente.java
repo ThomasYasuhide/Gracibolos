f8a53ed36d2851f008ecc8e6d3130ba7463ad196
Rogerio <Rogerio@172.20.22.222>
2016-04-26 22:22:19 -0300

    FOTOS upload e teste Junit

---
+++ Gracibolos/src/main/java/br/com/gracibolos/jdbc/teste/TesteCliente.java
@@ -0,0 +1,54 @@
+package br.com.gracibolos.jdbc.teste;
+
+import static org.junit.Assert.*;
+
+import java.time.LocalDate;
+
+import javax.swing.text.StyledEditorKit.BoldAction;
+
+import org.junit.Test;
+
+import br.com.gracibolos.jdbc.dao.ClienteDao;
+import br.com.gracibolos.jdbc.model.Cliente;
+
+public class TesteCliente {
+
+	@Test
+	public void test() {
+Cliente c1 = new Cliente();
+		
+		c1.setStatus(1);
+		c1.setTipopessoa(2);
+		c1.setSexo(1);
+		c1.setDatanascimento(LocalDate.now());
+		c1.setNomerazao("");
+		c1.setCpfcnpj("");
+		c1.setRgie("");
+		c1.setEndereco("");		
+		c1.setNumero("54");
+		c1.setComplemento("10");
+		c1.setBairro("");
+		c1.setEstado(3);
+		c1.setCidade(3);		
+		c1.setCep("3678000");
+		c1.setTel("");
+		c1.setCel("");		
+		c1.setEmail("Raul@hotmail.com");
+		c1.setSite("teste");
+		c1.setClientedesde(LocalDate.now());		
+		c1.setObs("teste");
+		
+		ClienteDao dao = new ClienteDao();
+		Boolean b = true;
+		try {
+			assertEquals(b, dao.inserir(c1));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	
+
+	
+	}
+}
