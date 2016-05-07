48002a0587463b3bb07b44f9639711fb5c9686f5
RogerioHorauti <rogeriohorauti@gmail.com>
2016-05-07 09:48:29 -0300

    json matéria prima e fornecedor

--- Gracibolos/src/main/java/br/com/gracibolos/controller/ServerRestController.java
+++ Gracibolos/src/main/java/br/com/gracibolos/controller/ServerRestController.java
@@ -8,8 +8,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.gracibolos.jdbc.dao.ClienteDao;
+import br.com.gracibolos.jdbc.dao.FornecedorDao;
+import br.com.gracibolos.jdbc.dao.MateriaPrimaDao;
 import br.com.gracibolos.jdbc.dao.ProdutoDao;
 import br.com.gracibolos.jdbc.model.Cliente;
+import br.com.gracibolos.jdbc.model.Fornecedor;
+import br.com.gracibolos.jdbc.model.MateriaPrima;
 import br.com.gracibolos.jdbc.model.Produto;
 
 @RestController
@@ -20,8 +24,42 @@ public class ServerRestController {
 	private ClienteDao clienteDao;
 	private List<Produto> produtosList;
 	private List<Cliente> clientesList;
+	private MateriaPrimaDao mpDao;
+	private List<MateriaPrima> mpList;
+	private FornecedorDao fornDao;
+	private List<Fornecedor> fornList;
 	
-	@RequestMapping(value = "/rest-produtos", method = RequestMethod.GET, headers="Accept=application/json")
+	
+	@RequestMapping(value = "/rest-pesquisa-fornecedor/{pesquisa}", 
+			method = RequestMethod.GET, headers="Accept=application/json")  
+	public List<Fornecedor> pesquisar_fornecedor(@PathVariable String pesquisa) {
+		fornList = null;
+		fornDao = new FornecedorDao();
+		try {
+			fornList = fornDao.pesquisar(pesquisa);
+		} catch (Exception e) {
+			System.out.println("ERRO - rest fornecedor, pesquisa fornecedor.");
+			e.printStackTrace();
+		}    
+		return fornList;  
+	}
+	
+	@RequestMapping(value = "/rest-pesquisa-materiaprima/{pesquisa}", 
+			method = RequestMethod.GET, headers="Accept=application/json")  
+	public List<MateriaPrima> pesquisar_materiprima(@PathVariable String pesquisa) {
+		mpList = null;
+		mpDao = new MateriaPrimaDao();
+		try {
+			mpList = mpDao.pesquisar(pesquisa);
+		} catch (Exception e) {
+			System.out.println("ERRO - rest materiaprima, pesquisa materiaprima.");
+			e.printStackTrace();
+		}    
+		return mpList;  
+	}
+	
+	@RequestMapping(value = "/rest-produtos", 
+			method = RequestMethod.GET, headers="Accept=application/json")
 	public List<Produto> listAllProdutos() {
 		try {
 			produtoDao = new ProdutoDao();
@@ -34,7 +72,8 @@ public class ServerRestController {
     }
 	
 	
-	@RequestMapping(value = "/rest-pesquisar-produtos/{id}", method = RequestMethod.GET, headers="Accept=application/json")  
+	@RequestMapping(value = "/rest-pesquisar-produtos/{id}", 
+			method = RequestMethod.GET, headers="Accept=application/json")  
 	 public Produto findById(@PathVariable int id) {  
 		produtoDao = new ProdutoDao();
 		p = new Produto();
@@ -47,7 +86,8 @@ public class ServerRestController {
 	  return p;  
 	 }
 	
-	@RequestMapping(value = "/rest-pesquisa-produtos/{pesquisa}", method = RequestMethod.GET, headers="Accept=application/json")  
+	@RequestMapping(value = "/rest-pesquisa-produtos/{pesquisa}", 
+			method = RequestMethod.GET, headers="Accept=application/json")  
 	public List<Produto> pesquisar_produto(@PathVariable String pesquisa) {
 		produtoDao = null;
 		produtoDao = new ProdutoDao();
@@ -61,7 +101,8 @@ public class ServerRestController {
 	}
 
 	
-	@RequestMapping(value = "/rest-clientes/{nome}", method = RequestMethod.GET, headers="Accept=application/json")
+	@RequestMapping(value = "/rest-clientes/{nome}", 
+			method = RequestMethod.GET, headers="Accept=application/json")
 	public List<Cliente> ListClientes(@PathVariable String nome){
 		clienteDao = new ClienteDao();
 		try {
